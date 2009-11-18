54fbc0a72d1140eef1d8b57ef4553f0d05519ae2
Steve Lloyd <steve@repeatingbeats.com>
2009-11-17 23:35:54 -0600

    listener component registration

---
+++ birdplaydar/components/sbIPlaydarServiceListener/src/sbPlaydarServiceListener.js
@@ -0,0 +1,53 @@
+
+/*
+ * sbIPlaydarServiceListener - empty implementation for component registration
+ */
+
+if(typeof(Cc)=="undefined")
+  var Cc = Components.classes;
+if(typeof(Ci)=="undefined")
+  var Ci = Components.interfaces;
+if(typeof(Cu)=="undefined")
+  var Cu = Components.utils;
+if(typeof(Cr)=="undefined")
+  var Cr = Components.results;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");  
+Cu.import("resource://app/jsmodules/sbProperties.jsm");
+Cu.import("resource://app/jsmodules/sbLibraryUtils.jsm");
+
+// XPCOM component details
+const DESCRIPTION = "Playdar Service Listener";
+const CID         = "{1c761454-b795-418e-8497-4ece9d20107f}";
+const CONTRACTID  = "@repeatingbeats.com/playdar/playdar-service-listener;1";
+
+// XPCOM component constructor
+function sbIPlaydarServiceListener() {
+};
+
+sbIPlaydarServiceListener.prototype.constructor = sbIPlaydarServiceListener;
+
+sbIPlaydarServiceListener.prototype = {
+
+  // XPCOM details
+  classDescription: DESCRIPTION,
+  classID:  Components.ID(CID),
+  contractID:  CONTRACTID,
+
+  QueryInterface : XPCOMUtils.generateQI([Ci.sbIPlaydarServiceListener]),
+
+  clientID : null,
+  
+  onStat : function(detected) {
+
+  },
+
+  onResults : function(response,finalAnswer) {
+
+  }
+
+};
+
+function NSGetModule(compMgr, fileSpec) {
+  return XPCOMUtils.generateModule([sbIPlaydarServiceListener]);
+}
